56b6bc82e01a8e2d6133cd827f6d9b23f489e4b2
levkesha <zxcv02@gmail.com>
2022-09-26 13:07:18 +0300

    salt #2

--- app1/main.py
+++ app1/main.py
@@ -2,35 +2,10 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-jobs = ['first', 'second', 'third', 'fourth', 'fifth']
 
 
-@app.route('/')
-def home():
-    return "<h1>Bookjobs</h1><p>This site is a prototype API for creating jobs.</p>"
-
-
-@app.route('/post', methods=['POST'])
+@app.route('/', methods=['POST'])
 def api_post():
-    name = str(request.args["name"])
-    jobs.append(name)
-    return '', 200
-
-
-@app.route('/get', methods=['GET'])
-def api_get():
-    return jsonify(jobs)
-
-
-@app.route('/delete', methods=['POST'])
-def api_delete():
-    if 'name' in request.args:
-        name = request.args['name']
-        id = jobs.index(name)
-    else:
-        return "Error: No name name provided. Please specify an name."
-    jobs.pop(id)
-    return '', 200
-
+    return "Your Request was: " + request.__str__()
 
 app.run(host='0.0.0.0')
